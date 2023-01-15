1f33d3ba97e1b4ab6b7154fb9d33f913389eb6fc
Gary Sun <sungarysun@gmail.com>
2023-01-14 18:53:04 -0800

    Initial Commit

---
+++ iosVolunteerApp/iosVolunteerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosVolunteerAppApp.swift
+//  iosVolunteerApp
+//
+//  Created by Gary Sun on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct iosVolunteerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
